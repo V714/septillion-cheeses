89e35b89e09a08bf3ed37910f0c3e972fa347918
Tomasz <vlipczynski@gmail.com>
2021-10-15 19:49:36 +0200

    google analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-K81PPEVTWS";
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <React.StrictMode>
